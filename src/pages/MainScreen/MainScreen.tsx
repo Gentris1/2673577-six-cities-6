6c8ceeeb324c3f72f0b3136edf4e6e29af5b49ee
yura <yura.romashov000@mail.ru>
2025-09-28 13:49:59 +0500

    переименовал файлы и папки, деструктуризировал пропс

--- src/pages/MainScreen/MainScreen.tsx
+++ src/pages/MainScreen/MainScreen.tsx
@@ -1,4 +1,4 @@
-import PlaceCard from '../../components/place-card/place-card.tsx';
+import PlaceCard from '../../components/PlaceCard/PlaceCard.tsx';
 
 const PlaceCardType = {
   apartment: 'Apartment',
